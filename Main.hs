af6d8d63229171798931fa4d7c2d5b65b5ea1465
Samuel Schlesinger <samuel@simspace.com>
2020-06-09 18:10:08 -0400

    Fixed to verify signature of sender, updated Mac executable

--- Main.hs
+++ Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DataKinds #-}
@@ -6,17 +9,26 @@
 module Main where
 
 import Data.Time.Clock
-import Data.Binary (encodeFile, decodeFile, Binary(get, put))
+import Data.Binary (encodeFile, decodeFile, Binary(get, put), encode, decodeOrFail)
 import Crypto.PubKey.RSA (generate, PrivateKey(..), PublicKey(..))
 import Crypto.PubKey.RSA.OAEP (encrypt, decrypt, defaultOAEPParams)
+import Crypto.PubKey.RSA.PSS (sign, defaultPSSParams, verify)
 import Crypto.Hash (SHA512(SHA512))
 import Options.Commander
 import qualified Data.ByteString.Char8 as BS8
+import qualified GHC.Generics
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 import Control.Monad (when)
 
 newtype PrivateKeyFile = PrivateKeyFile { unPrivateKeyFile :: PrivateKey }
 newtype PublicKeyFile = PublicKeyFile { unPublicKeyFile :: PublicKey }
+
+data SecretMessage = SecretMessage
+  { signature :: BS.ByteString
+  , message :: BS.ByteString
+  } deriving stock GHC.Generics.Generic
+    deriving anyclass Binary
 
 instance Binary PrivateKeyFile where
   put (PrivateKeyFile PrivateKey{..}) = do
@@ -39,36 +51,76 @@ instance Binary PublicKeyFile where
 type CMD = 
   Named "messaging"
   & Flag "verbose"
-  &   (("key" & ("new" & (Arg "keyPairName" [Char] & Raw)))
-    + (("write" & (Arg "publicKey" [Char] & (Arg "secretMessage" String & Raw)))
-    + ("read" & (Arg "privateKey" [Char] & (Arg "secretMessageFile" FilePath & Raw)))))
+  & (
+    "key"   & "new" 
+            & Arg "name" [Char]
+            & Raw
+
+  + "write" & Arg "me" [Char] 
+            & Arg "you" [Char]
+            & Arg "file" String
+            & Arg "message" String 
+            & Raw
+
+  + "read"  & Arg "me" [Char]
+            & Arg "you" [Char]
+            & Arg "file" FilePath
+            & Raw
+  )
 
 program :: ProgramT CMD IO ()
-program = named @"messaging" $ flag @"verbose" $ \verbose -> keypair verbose :+: author verbose :+: read verbose
+program = named @"messaging" $ flag @"verbose" $ \verbose -> keypair verbose :+: write verbose :+: read verbose
   where
-    read verbose = sub @"read" $ arg @"privateKey" \privateKeyFile -> arg @"secretMessageFile" \source -> raw do
-      when verbose $ putStrLn "reading message"
-      privateKey <- unPrivateKeyFile <$> decodeFile (privateKeyFile <> ".private")
-      bs <- BS.readFile source
-      let decrypted = decrypt Nothing (defaultOAEPParams SHA512) privateKey bs
-      case decrypted of
-        Left e -> do
-          when verbose $ putStr "decryption error:" >> print e
-        Right untwisted -> putStrLn $ BS8.unpack untwisted
-    author verbose = sub @"write" $ arg @"publicKey" \to -> arg @"secretMessage" \msg -> raw do
-      putStrLn "authoring message"
-      time <- getCurrentTime
-      publicKey <- unPublicKeyFile <$> decodeFile (to <> ".public")
-      encrypted <- encrypt (defaultOAEPParams SHA512) publicKey (BS8.pack msg)
-      case encrypted of
-        Left e -> do
-          when verbose $ putStr "encryption error: " >> print e
-        Right twisted -> BS.writeFile (to <> ":" <> show time) twisted 
-    keypair verbose = sub @"key" $ sub @"new" $ arg @"keyPairName" \name -> raw do
-      when verbose $ putStrLn "generating keypair"
-      (publicKey, privateKey) <- generate 2048 0x10001
-      encodeFile (name <> ".public") (PublicKeyFile publicKey)
-      encodeFile (name <> ".private") (PrivateKeyFile privateKey)
+    read verbose =
+      sub @"read" 
+      $ arg @"me" \me -> 
+        arg @"you" \you ->
+        arg @"file" \source -> raw do
+          when verbose $ putStrLn "reading message"
+          privateKey <- unPrivateKeyFile <$> decodeFile (me <> ".private")
+          publicKey <- unPublicKeyFile <$> decodeFile (you <> ".public")
+          bs <- BS.readFile source
+          case decodeOrFail (BSL.fromStrict bs) of
+            Left e -> do
+              when verbose $ putStrLn "decoding error:" >> print e
+            Right (_, _, SecretMessage sig msg) -> do
+                case decrypt Nothing (defaultOAEPParams SHA512) privateKey msg of
+                  Left e -> do
+                    when verbose $ putStr "decryption error:" >> print e
+                  Right untwisted ->
+                    if verify (defaultPSSParams SHA512) publicKey untwisted sig
+                      then putStrLn $ BS8.unpack untwisted
+                    else when verbose $ putStrLn "could not verify signature"
+
+    write verbose = 
+      sub @"write" 
+      $ arg \me ->
+        arg \you ->
+        arg \file ->
+        arg \message -> raw do
+          let bsMsg = BS8.pack message
+          when verbose $ putStrLn "authoring message"
+          privateKey <- unPrivateKeyFile <$> decodeFile (me <> ".private")
+          publicKey <- unPublicKeyFile <$> decodeFile (you <> ".public")
+          signature <- sign Nothing (defaultPSSParams SHA512) privateKey bsMsg
+          case signature of
+            Left e -> do
+              when verbose $ putStr "signature error: " >> print e
+            Right sig -> do
+              encryptedMsg <- encrypt (defaultOAEPParams SHA512) publicKey bsMsg
+              case encryptedMsg of
+                Left e -> do
+                  when verbose $ putStr "encryption error: " >> print e
+                Right twisted -> encodeFile file (SecretMessage sig twisted)
+
+    keypair verbose =
+      sub @"key" 
+      $ sub @"new" 
+      $ arg \name -> raw do
+          when verbose $ putStrLn "generating keypair"
+          (publicKey, privateKey) <- generate 2048 0x10001
+          encodeFile (name <> ".public") (PublicKeyFile publicKey)
+          encodeFile (name <> ".private") (PrivateKeyFile privateKey)
 
 main :: IO ()
 main = command_ (program :+: sub @"help" (usage @CMD) :+: raw (putStrLn "try: messaging help"))
